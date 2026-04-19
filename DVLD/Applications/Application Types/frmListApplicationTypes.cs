e6298f2851f8f7319b5ee6cfdf313ee015bf5cc4
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-04-19 11:50:39 +0300

    feat(ui): add frmListApplicationTypes

---
+++ DVLD/Applications/Application Types/frmListApplicationTypes.cs
@@ -0,0 +1,56 @@
+﻿using DVLD_Business;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Applications.Application_Types
+{
+    public partial class frmListApplicationTypes : Form
+    {
+        private DataTable _dtAllApplicationTypes;
+
+        public frmListApplicationTypes()
+        {
+            InitializeComponent();
+        }
+
+        private void _RefreshNumberOfRecords()
+        {
+            lblRecordsCount.Text = _dtAllApplicationTypes.Rows.Count.ToString();
+        }
+
+        private void _RefreshData()
+        {
+            _dtAllApplicationTypes = clsApplicationType.GetAllApplicationTypes();
+            dgvApplicationTypes.DataSource = _dtAllApplicationTypes;
+        }
+
+        private void frmListApplicationTypes_Load(object sender, EventArgs e)
+        {
+            _RefreshData();
+
+            if(dgvApplicationTypes.Rows.Count > 0)
+            {
+                dgvApplicationTypes.Columns[0].HeaderText = "ID";
+                dgvApplicationTypes.Columns[0].Width = 110;
+
+                dgvApplicationTypes.Columns[1].HeaderText = "Title";
+                dgvApplicationTypes.Columns[1].Width = 400;
+
+                dgvApplicationTypes.Columns[2].HeaderText = "Fees";
+                dgvApplicationTypes.Columns[2].Width = 100;
+            }
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
